f1cb0e62ac671f8a1714a119281342a8796de570
Gutnar Leede <gutnar@gmail.com>
2024-12-18 16:26:12 +0200

    Add IR recording and separate resolutions for video streams

--- src/record_orbbec.cc
+++ src/record_orbbec.cc
@@ -7,6 +7,10 @@
 #include <libobsensor/hpp/Context.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <thread>
+#include <condition_variable>
 
 namespace fs = ghc::filesystem;
 
@@ -25,16 +29,16 @@ int main(int argc, char *argv[])
     auto device_index = op.add<popl::Value<unsigned int>>("d", "device", "device index", 0);
     auto output_path = op.add<popl::Value<std::string>>("o", "output", "output folder path");
     auto force = op.add<popl::Switch>("f", "force", "force overwrite output folder");
-    auto color_width = op.add<popl::Value<unsigned int>>("", "color-width", "color image width", 1280);
-    auto color_height = op.add<popl::Value<unsigned int>>("", "color-height", "color image height", 960);
-    auto color_fps = op.add<popl::Value<unsigned int>>("", "color-fps", "color image fps", 30);
+    auto color_size = op.add<popl::Value<std::string>>("", "color-size", "color image resolution (hd, 960p, fhd, qhd, 4k)", "hd");
     auto color_exposure = op.add<popl::Value<unsigned int>>("", "color-exposure", "color exposure");
     auto color_gain = op.add<popl::Value<unsigned int>>("", "color-gain", "color gain", 0);
+    auto color_format = op.add<popl::Value<std::string>>("", "color-format", "color image format (jpg, png)", "jpg");
     auto white_balance = op.add<popl::Value<unsigned int>>("", "white-balance", "white balance");
-    auto depth_width = op.add<popl::Value<unsigned int>>("", "depth-width", "depth image width", 640);
-    auto depth_height = op.add<popl::Value<unsigned int>>("", "depth-height", "depth image height", 576);
-    auto depth_fps = op.add<popl::Value<unsigned int>>("", "depth-fps", "depth image fps", 30);
+    auto depth_size = op.add<popl::Value<std::string>>("", "depth-size", "depth image resolution (288p, 512p, 576p, 1024p)", "576p");
+    auto fps = op.add<popl::Value<unsigned int>>("", "fps", "fps", 30);
     auto depth_threshold = op.add<popl::Value<float>>("", "depth-threshold", "depth threshold", 40.0);
+    auto viewer = op.add<popl::Switch>("", "viewer", "enable viewer");
+    auto wait = op.add<popl::Value<unsigned int>>("", "wait", "wait time in seconds for auto exposure", 5);
 
     try
     {
@@ -54,6 +58,7 @@ int main(int argc, char *argv[])
         std::cerr << op << std::endl;
         return EXIT_FAILURE;
     }
+
     if (!op.unknown_options().empty())
     {
         for (const auto &unknown_option : op.unknown_options())
@@ -63,11 +68,97 @@ int main(int argc, char *argv[])
         std::cerr << op << std::endl;
         return EXIT_FAILURE;
     }
+
     if (!output_path->is_set())
     {
         std::cerr << "Error: please provide an output path" << std::endl;
         std::cerr << std::endl;
         std::cerr << op << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Get color resolution
+    unsigned int color_width, color_height;
+
+    if (color_size->value() == "hd")
+    {
+        color_width = 1280;
+        color_height = 720;
+    }
+    else if (color_size->value() == "960p")
+    {
+        color_width = 1280;
+        color_height = 960;
+    }
+    else if (color_size->value() == "fhd")
+    {
+        color_width = 1920;
+        color_height = 1080;
+    }
+    else if (color_size->value() == "qhd")
+    {
+        color_width = 2560;
+        color_height = 1440;
+    }
+    else if (color_size->value() == "4k")
+    {
+        color_width = 3840;
+        color_height = 2160;
+    }
+    else
+    {
+        std::cerr << "Error: invalid color size, must be hd, 960p, fhd, qhd, 4k" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Get depth resolution
+    unsigned int depth_width, depth_height;
+    float depth_range_max;
+
+    if (depth_size->value() == "288p") // binned
+    {
+        depth_width = 320;
+        depth_height = 288;
+        depth_range_max = 5.46;
+    }
+    else if (depth_size->value() == "512p") // binned
+    {
+        depth_width = 512;
+        depth_height = 512;
+        depth_range_max = 2.88;
+    }
+    else if (depth_size->value() == "576p") // unbinned
+    {
+        depth_width = 640;
+        depth_height = 576;
+        depth_range_max = 3.86;
+    }
+    else if (depth_size->value() == "1024p") // unbinned
+    {
+        depth_width = 1024;
+        depth_height = 1024;
+        depth_range_max = 2.21;
+    }
+    else
+    {
+        std::cerr << "Error: invalid depth size, must be 288p, 512p, 576p, 1024p" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    // Get color format
+    OBFormat ob_color_format;
+
+    if (color_format->value() == "jpg")
+    {
+        ob_color_format = OB_FORMAT_MJPG;
+    }
+    else if (color_format->value() == "png")
+    {
+        ob_color_format = OB_FORMAT_RGB;
+    }
+    else
+    {
+        std::cerr << "Error: invalid color format, must be jpg or png" << std::endl;
         return EXIT_FAILURE;
     }
 
@@ -96,6 +187,7 @@ int main(int argc, char *argv[])
     fs::create_directories(config_path.parent_path());
     fs::create_directories(fs::path(output_path->value()) / "rgb");
     fs::create_directories(fs::path(output_path->value()) / "depth");
+    fs::create_directories(fs::path(output_path->value()) / "ir");
 
     // Open the device
     ob::Context ctx;
@@ -108,11 +200,11 @@ int main(int argc, char *argv[])
     }
 
     auto dev = devList->getDevice(device_index->value());
-
     auto devConfig = std::make_shared<ob::Config>();
-    devConfig->enableVideoStream(OB_STREAM_COLOR, color_width->value(), color_height->value(), color_fps->value(), OB_FORMAT_MJPG);
-    devConfig->enableVideoStream(OB_STREAM_DEPTH, depth_width->value(), depth_height->value(), depth_fps->value(), OB_FORMAT_Y16);
-    devConfig->setAlignMode(ALIGN_D2C_SW_MODE);
+    devConfig->enableVideoStream(OB_STREAM_COLOR, color_width, color_height, fps->value(), OB_FORMAT_RGB888);
+    devConfig->enableVideoStream(OB_STREAM_DEPTH, depth_width, depth_height, fps->value(), OB_FORMAT_Y16);
+    devConfig->enableVideoStream(OB_STREAM_IR, depth_width, depth_height, fps->value(), OB_FORMAT_Y16);
+    // devConfig->setAlignMode(ALIGN_D2C_SW_MODE);
 
     // apply exposure settings
     dev->setBoolProperty(OB_PROP_COLOR_AUTO_EXPOSURE_BOOL, !color_exposure->is_set());
@@ -138,28 +230,10 @@ int main(int argc, char *argv[])
     pipe.enableFrameSync();
     pipe.start(devConfig);
 
-    // Get depth range based on depth resolution
-    float depth_range_max;
-
-    switch (depth_width->value())
-    {
-    default:
-        depth_range_max = 2.21;
-        break;
-    case 512:
-        depth_range_max = 2.88;
-        break;
-    case 640:
-        depth_range_max = 3.86;
-        break;
-    case 320:
-        depth_range_max = 5.46;
-        break;
-    }
-
     // write vslam config
     auto params = pipe.getCameraParam();
     fs::ofstream config_file(config_path);
+
     config_file << "Camera:\n";
     config_file << "  name: \"Orbbec\"\n";
     config_file << "  setup: \"RGBD\"\n";
@@ -176,14 +250,31 @@ int main(int argc, char *argv[])
     config_file << "  p2: " << params.rgbDistortion.p2 << "\n";
     config_file << "  k3: " << params.rgbDistortion.k3 << "\n";
     config_file << "\n";
-    config_file << "  fps: " << color_fps->value() << "\n";
-    config_file << "  cols: " << color_width->value() << "\n";
-    config_file << "  rows: " << color_height->value() << "\n";
+    config_file << "  fps: " << fps->value() << "\n";
+    config_file << "  cols: " << color_width << "\n";
+    config_file << "  rows: " << color_height << "\n";
     config_file << "  color_order: \"RGB\"\n";
     config_file << "\n";
     config_file << "  focal_x_baseline: " << (depth_range_max / depth_threshold->value() * params.rgbIntrinsic.fx) << "\n";
     config_file << "  depth_threshold: " << depth_threshold->value() << "\n";
     config_file << "\n";
+
+    config_file << "DepthCamera:\n";
+    config_file << "  fx: " << params.depthIntrinsic.fx << "\n";
+    config_file << "  fy: " << params.depthIntrinsic.fy << "\n";
+    config_file << "  cx: " << params.depthIntrinsic.cx << "\n";
+    config_file << "  cy: " << params.depthIntrinsic.cy << "\n";
+    config_file << "\n";
+    config_file << "  k1: " << params.depthDistortion.k1 << "\n";
+    config_file << "  k2: " << params.depthDistortion.k2 << "\n";
+    config_file << "  p1: " << params.depthDistortion.p1 << "\n";
+    config_file << "  p2: " << params.depthDistortion.p2 << "\n";
+    config_file << "  k3: " << params.depthDistortion.k3 << "\n";
+    config_file << "\n";
+    config_file << "  cols: " << depth_width << "\n";
+    config_file << "  rows: " << depth_height << "\n";
+    config_file << "\n";
+
     config_file << "Preprocessing:\n";
     config_file << "  min_size: 800\n";
     config_file << "  depthmap_factor: 1000.0\n";
@@ -193,6 +284,7 @@ int main(int argc, char *argv[])
     fs::ofstream rgb_txt(fs::path(output_path->value()) / "rgb.txt");
     fs::ofstream depth_txt(fs::path(output_path->value()) / "depth.txt");
     fs::ofstream imu_txt(fs::path(output_path->value()) / "imu.txt");
+    fs::ofstream ir_txt(fs::path(output_path->value()) / "ir.txt");
 
     rgb_txt << "# color images" << std::endl;
     rgb_txt << "# timestamp [s] | filename" << std::endl;
@@ -202,6 +294,9 @@ int main(int argc, char *argv[])
 
     imu_txt << "# accelerometer and gyroscope data" << std::endl;
     imu_txt << "# timestamp [s] | acc_x [m/s^2] | acc_y [m/s^2] | acc_z [m/s^2] | gyro_x [rad/s] | gyro_y [rad/s] | gyro_z [rad/s]" << std::endl;
+
+    ir_txt << "# ir images" << std::endl;
+    ir_txt << "# timestamp [s] | filename" << std::endl;
 
     // Queue accelerometer and gyroscope data to combine by timestamps in main thread
     std::mutex imu_mutex;
@@ -247,8 +342,78 @@ int main(int argc, char *argv[])
             } });
     }
 
+    // // Write to disk on separate thread
+    // std::deque<std::pair<std::string, std::vector<uchar>>> write_queue;
+    // std::mutex write_mutex;
+    // std::condition_variable write_cv;
+
+    // auto write_thread = std::thread([&]()
+    //                                 {
+    //     while (running)
+    //     {
+    //         std::pair<std::string, std::vector<uchar>> item;
+
+    //         {
+    //             std::unique_lock<std::mutex> lock(write_mutex);
+    //             write_cv.wait(lock, [&]()
+    //                           { return !write_queue.empty() || !running; });
+
+    //             if (!running)
+    //             {
+    //                 break;
+    //             }
+
+    //             item = write_queue.front();
+    //             write_queue.pop_front();
+    //         }
+
+    //         // Write to disk
+    //         fs::ofstream file(item.first, std::ios::binary);
+    //         file.write(reinterpret_cast<const char *>(item.second.data()), item.second.size());
+    //         file.close();
+    //     } });
+
+    // Compress images on separate thread
+    std::deque<std::pair<std::string, cv::Mat>> compress_queue;
+    std::mutex compress_mutex;
+    std::condition_variable compress_cv;
+
+    auto compress_thread = std::thread([&]()
+                                       {
+        while (running)
+        {
+            std::pair<std::string, cv::Mat> item;
+
+            {
+                std::unique_lock<std::mutex> lock(compress_mutex);
+                compress_cv.wait(lock, [&]()
+                                 { return !compress_queue.empty() || !running; });
+
+                if (!running)
+                {
+                    break;
+                }
+
+                item = compress_queue.front();
+                compress_queue.pop_front();
+            }
+
+            cv::imwrite(item.first, item.second);
+
+            // // Convert to jpg or png and send to write queue
+            // auto format = item.first.substr(item.first.find_last_of("."));
+            // std::vector<uchar> buf;
+            // cv::imencode(format, item.second, buf);
+
+            // {
+            //     std::lock_guard<std::mutex> lock(write_mutex);
+            //     write_queue.push_back({item.first, buf});
+            //     write_cv.notify_one();
+            // }
+        } });
+
     // Record color and depth frames
-    cv::Mat depth_mat(color_height->value(), color_width->value(), CV_16UC1);
+    auto start_time = std::chrono::high_resolution_clock::now();
 
     while (running)
     {
@@ -285,6 +450,9 @@ int main(int argc, char *argv[])
             }
         }
 
+        // Queue items
+        std::vector<std::pair<std::string, cv::Mat>> queue_items;
+
         // Write color as jpeg
         auto color_frame = frame_set->colorFrame();
 
@@ -292,12 +460,23 @@ int main(int argc, char *argv[])
         {
             auto timestamp = color_frame->timeStamp();
             auto timestamp_str = std::to_string(timestamp / 1e3);
-            const auto color_path = fs::path(output_path->value()) / "rgb" / (timestamp_str + ".jpg");
-            fs::ofstream color_file(color_path, std::ios::binary);
-            color_file.write(reinterpret_cast<const char *>(color_frame->data()), color_frame->dataSize());
-            color_file.close();
+            const auto color_path = fs::path(output_path->value()) / "rgb" / (timestamp_str + "." + color_format->value());
 
+            // Write as jpg or png
+            cv::Mat color_mat(color_frame->height(), color_frame->width(), CV_8UC3, color_frame->data());
+            cv::cvtColor(color_mat, color_mat, cv::COLOR_RGB2BGR);
+            // cv::imwrite(color_path, color_mat);
+            queue_items.push_back({color_path, color_mat});
+
+            // Write to txt file
             rgb_txt << timestamp_str << " rgb/" << color_path.filename().string() << std::endl;
+
+            // View color image
+            if (viewer->is_set())
+            {
+                cv::imshow("Color", color_mat.clone());
+                cv::waitKey(1);
+            }
         }
 
         // Write depth as png
@@ -307,11 +486,65 @@ int main(int argc, char *argv[])
         {
             auto timestamp = depth_frame->timeStamp();
             auto timestamp_str = std::to_string(timestamp / 1e3);
-            memcpy(depth_mat.data, depth_frame->data(), depth_frame->dataSize());
-            const auto depth_path = fs::path(output_path->value()) / "depth" / (timestamp_str + ".png");
-            cv::imwrite(depth_path, depth_mat);
 
+            // Write as png
+            cv::Mat depth_mat(depth_frame->height(), depth_frame->width(), CV_16UC1, depth_frame->data());
+            const auto depth_path = fs::path(output_path->value()) / "depth" / (timestamp_str + ".png");
+            // cv::imwrite(depth_path, depth_mat);
+            queue_items.push_back({depth_path, depth_mat});
+
+            // Write to txt file
             depth_txt << timestamp_str << " depth/" << depth_path.filename().string() << std::endl;
+
+            // View depth image
+            if (viewer->is_set())
+            {
+                cv::Mat depth_mat_8u;
+                cv::normalize(depth_mat, depth_mat_8u, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+                cv::imshow("Depth", depth_mat_8u);
+                cv::waitKey(1);
+            }
+        }
+
+        // Write ir as png
+        auto ir_frame = frame_set->irFrame();
+
+        if (ir_frame != nullptr)
+        {
+            auto timestamp = ir_frame->timeStamp();
+            auto timestamp_str = std::to_string(timestamp / 1e3);
+
+            // Write as png
+            cv::Mat ir_mat(ir_frame->height(), ir_frame->width(), CV_16UC1, ir_frame->data());
+            const auto ir_path = fs::path(output_path->value()) / "ir" / (timestamp_str + ".png");
+            // cv::imwrite(ir_path, ir_mat);
+            queue_items.push_back({ir_path, ir_mat});
+
+            // Write to txt file
+            ir_txt << timestamp_str << " ir/" << ir_path.filename().string() << std::endl;
+
+            // View ir image
+            if (viewer->is_set())
+            {
+                cv::Mat ir_mat_8u;
+                cv::normalize(ir_mat, ir_mat_8u, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+                cv::imshow("IR", ir_mat_8u);
+                cv::waitKey(1);
+            }
+        }
+
+        // Queue items for compression if queue is empty and wait time has passed
+        auto current_time = std::chrono::high_resolution_clock::now();
+
+        if ((current_time - start_time) > std::chrono::seconds(wait->value()))
+        {
+            std::lock_guard<std::mutex> lock(compress_mutex);
+
+            if (compress_queue.empty())
+            {
+                compress_queue.insert(compress_queue.end(), queue_items.begin(), queue_items.end());
+                compress_cv.notify_one();
+            }
         }
     }
 
@@ -332,6 +565,9 @@ int main(int argc, char *argv[])
     rgb_txt.close();
     depth_txt.close();
     imu_txt.close();
+
+    // Close opencv windows
+    cv::destroyAllWindows();
 
     return 0;
 }
